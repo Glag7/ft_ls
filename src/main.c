54d6ab6f4d1c3c77a4cd10ba35d1ae62d50629b3
Gabriel Laguyonie <glaguyon@3E5.42angouleme.fr>
2025-11-26 00:23:50 +0100

    wip file extraction

--- src/main.c
+++ src/main.c
@@ -16,7 +16,90 @@ hidden files bool
 human sizes bool
 */
 
-#include "utils.h"
+//check ls -d .
+
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+
+void	list_files(const char *dirname)
+{
+	DIR*	dir = opendir(dirname);
+	struct dirent	*file;
+
+	if (dir == NULL)
+		return;//errno
+	errno = 0;
+	file = readdir(dir);
+	while (file)
+	{
+		printf("//%s\n", file->d_name);
+		errno = 0;
+		file = readdir(dir);
+	}
+	if (errno)
+		return;//error
+	return;
+}
+
+typedef struct fopts_s
+{
+	bool	hidden;
+	bool	owner;
+	bool	data;
+	bool	dir_as_file;
+}	fopts_t;
+
+typedef struct	finfo_s
+{
+	char				*name;
+	bool				isdir;
+	gid_t				group;
+	uid_t				owner;
+	nlink_t				nlinks;
+	off_t				size;
+	bool				symlink;
+	mode_t				perms;
+	struct timespec		lastmod;
+}	finfo_t;
+
+finfo_t	*get_dir(const char *dirname, const fopts_t *opts)
+{
+	finfo_t			*files = NULL;
+	size_t			nfiles = 0;
+	DIR*			dir = opendir(dirname);
+	struct dirent	*file;
+
+	if (dir == NULL)
+		return;//errno
+	errno = 0;
+	file = readdir(dir);
+	while (file)
+	{
+		//check malloc size for realloc, null at the end
+		//fill file data
+		errno = 0;
+		file = readdir(dir);
+	}
+	if (errno)
+		return;//error
+	return;
+}
+
+//XXX store data like size etc for printing
+//print_dir:
+//get files with x data (hidden)
+//sort them
+//print everything (if -R stop printing to call print_dir again)
+//
+
+void	fill_file_opts(const char *opts, fopts_t *file_opts)
+{
+	file_opts->hidden = opts['a'];
+	file_opts->data = opts['l'] || opts['g'];
+	file_opts->owner = opts['l'] && !opts['g'];
+	file_opts->dir_as_file = opts['d'];
+}
 
 int	main(int argc, char **argv)
 {
@@ -24,10 +107,14 @@ int	main(int argc, char **argv)
 	char	opts[255] = {0};
 	int		err;
 
+	fopts_t	fopts;
+
 	err = extract_opts(argc, argv, opts);
 	if (err)
 		return err;
 	extract_args(argc, args);
+
+	fill_file_opts(opts, &fopts);
 	//fill some struct with opts
 	//list, sort, format, print
 	
@@ -43,5 +130,6 @@ int	main(int argc, char **argv)
 	{
 		printf("%s\n", args[i]);
 	}
+	list_files(*args);
 	return 0;
 }
